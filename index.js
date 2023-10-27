e3c50f5a9af0a066a9d5db7ab4fe67b261cb4190
EA - Thiago Teodoro Santana <thiago_teodoro@educadventista.org>
2023-10-27 10:05:31 -0300

    começo da aula de nodemon

---
+++ index.js
@@ -0,0 +1,10 @@
+const { application } = require("express")
+const express = require("express")
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Estou funcionando com express")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
