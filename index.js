425a9184cea5b9c1bbe2bea7fc99db521200a311
EA - Thiago Teodoro Santana <thiago_teodoro@educadventista.org>
2023-11-09 07:44:05 -0300

    Termino do código

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const { application } = require("express")
 const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
